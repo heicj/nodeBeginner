ecd264ca91ec8c48e3ce94c3f38e6eea60ac6956
Charlie <heinercj@gmail.com>
2018-04-08 09:44:30 -0700

    router set up extend start to take router.route param

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 var http = require("http");
+var url = require("url");
 
-http.createServer(function(re, res){
-    res.writeHead(200, {"Content-Type": "text/plain"});
-    res.write("Hello World");
-    res.end()
-}).listen(8888);
+function start(route){
+    function onRequest(req, res){
+        var pathname = url.parse(req.url).pathname;
+        console.log("Request for " + pathname + " received.");
+
+        route(pathname);
+        
+        res.writeHead(200, {"Content-Type": "text/plain"});
+        res.write("Hello World");
+        res.end()
+    }
+
+    http.createServer(onRequest).listen(8888);
+    console.log("Server has started");
+}
+
+exports.start = start;
