234539d75488256691764dbf9118a04aaac84e07
Charlie <heinercj@gmail.com>
2018-04-11 10:51:29 -0700

    function upload changed to handle img upload

--- requestHandlers.js
+++ requestHandlers.js
@@ -2,7 +2,7 @@ var exec = require("child_process").exec;
 var querystring = require("querystring");
 var fs = require("fs");
 
-function start(response, postData) {
+function start(response) {
     console.log("request handler 'start' was called.");
     
     var body = '<html>' +
@@ -11,8 +11,9 @@ function start(response, postData) {
         'charset=UTF-8 />'+
         '</hed>'+
         '<body>'+
-        '<form action="/upload" method="post">'+
+        '<form action="/upload" enctype="multipart/form-data" method="post">'+
         '<textarea name="text" rows="20" cols="60"></textarea>'+
+        '<input type="file" name="upload">'+
         '<input type="submit" value="Submit text" />'+
         '</form>'+
         '</body>'+
@@ -30,11 +31,27 @@ function start(response, postData) {
     // });
 }
 
-function upload(response, postData){
+function upload(response, request){
     console.log("request handler 'upload' was called.");
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("You've sent the text: " + querystring.parse(postData).text);
-    response.end();
+
+    var form = new formidable.IncomingForm();
+    console.log("about to parse");
+    form.parse(request, function(error, fields, files) {
+        console.log("parsing done");
+
+        //possible error on windows systems: tried to rename to an already existing file
+        fs.rename(files.upload.path, "./tmp/test.pgn", function(error) {
+            if (error) {
+                fs.unlink("./tmp/test.png");
+                fs.rename(files.upload.path, "./tmp/test.png");
+            }
+        });
+
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write("received image: <br/>");
+        response.write("<img src='/show' />");
+        response.end();
+    });
 }
 
 function show(response){
--- server.js
+++ server.js
@@ -3,20 +3,9 @@ var url = require("url");
 
 function start(route, handle){
     function onRequest(req, response){
-        var postData = "";
         var pathname = url.parse(req.url).pathname;
         console.log("Request for " + pathname + " received.");
-
-        req.setEncoding("utf8");
-
-        req.addListener("data", function(postDataChunk){
-            postData += postDataChunk;
-            console.log("Received POST data chunk '" + postDataChunk + "'.");
-        });
-        
-        req.addListener("end", function(){
-            route(handle, pathname, response, postData);
-        })
+        route(handle, pathname, response, request);
     }
 
     http.createServer(onRequest).listen(8888);
