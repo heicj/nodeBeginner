4346c4b6dd56bae24528daacd3095fa87e987eb6
Charlie <heinercj@gmail.com>
2018-04-11 10:07:15 -0700

    set up html body. sending post data down to request handler.

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,15 +1,31 @@
 var exec = require("child_process").exec;
 
-function start() {
+function start(response, postData) {
     console.log("request handler 'start' was called.");
     
-    var content = "empty"
+    var body = '<html>' +
+        '<head>'+
+        '<meta http-equiv="Content-Type" content="text/html; ' +
+        'charset=UTF-8 />'+
+        '</hed>'+
+        '<body>'+
+        '<form action="/upload" method="post">'+
+        '<textarea name="text" rows="20" cols="60"></textarea>'+
+        '<input type="submit" value="Submit text" />'+
+        '</form>'+
+        '</body>'+
+        '</html>';
 
-    exec("ls - lah", function(error, stdout, stderr){
-        response.writeHead(200, {"Content-Type": "text/plain"});
-        response.write(stdout);
+        response.writeHead(200, { "Content-Type": "text/html"});
+        response.write(body);
         response.end();
-    });
+    // var content = "empty"
+
+    // exec("ls - lah", function(error, stdout, stderr){
+    //     response.writeHead(200, {"Content-Type": "text/plain"});
+    //     response.write(stdout);
+    //     response.end();
+    // });
 }
 
 function upload(response){
